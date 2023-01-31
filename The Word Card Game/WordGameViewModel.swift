f82abba2b70c9b7117786314181cc9d94635f83d
Dalafiari Samuel <dalafiarisamuel@gmail.com>
2023-01-31 12:34:15 +0100

    update spelling error in WordGameViewModel

--- The Word Card Game/WordGameViewModel.swift
+++ The Word Card Game/WordGameViewModel.swift
@@ -17,7 +17,7 @@ extension ContentView {
         @Published var cpuScore: Int = 0
 
         private let availableCards: [Int] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-        private let availableBackground: [String] = ["cloth", "plain", "wood-carton", "wood-grain"]
+        private let availableBackground: [String] = ["cloth", "plain", "wood-cartoon", "wood-grain"]
        
         func deal() {
             let randomPlayerCard = "card\(availableCards.randomElement()!)"
